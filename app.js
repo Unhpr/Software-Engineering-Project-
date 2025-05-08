e93b34f4271046be311642ee26294240cd037dcd
Michael Lin <Michaellin012@gmail.com>
2025-05-08 17:51:03 +0100

    signup

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
